e58f2a59f1e44a43ad678f8a8cf40b63b83171c8
JDanielFV <119628732+JDanielFV@users.noreply.github.com>
2022-12-26 00:17:45 -0600

    prueba 0.1

--- scrypt.js
+++ scrypt.js
@@ -1,17 +1,29 @@
-// Encriptar texto
-function encrypt(text) {
+// Asignar controladores de eventos a los botones
+document.getElementById("botonEncriptar").onclick = function() {
+    // Obtener el texto de entrada y la salida
+    let entrada = document.getElementById("textoEntrada").value;
+    let salida = document.getElementById("textoSalida");
+  
+    // Llamar a la función de encriptación con el texto de entrada como argumento y asignar el resultado a la propiedad value del elemento de salida
+    salida.value = encrypt(entrada);
+  }
+  
+  document.getElementById("botonDesencriptar").onclick = function() {
+    // Obtener el texto de entrada y la salida
+    let entrada = document.getElementById("textoEntrada").value;
+    let salida = document.getElementById("textoSalida");
+  
+    // Llamar a la función de desencriptación con el texto de entrada como argumento y asignar el resultado a la propiedad value del elemento de salida
+    salida.value = decrypt(entrada);
+  }
+  
+  // Función de encriptación
+  function encrypt(text) {
     return btoa(text);
   }
-
-  var texto = 
   
-  // Desencriptar texto
+  // Función de desencriptación
   function decrypt(text) {
     return atob(text);
   }
-
-  // Obtén el texto a encriptar del elemento <textarea> con id="entrada"
-  let text = document.getElementById("entrada").value;
-
-  // Llamar a la función de encriptación con el texto como argumento y asignar el resultado a la propiedad value del elemento <textarea> con id="salida"
-  document.getElementById("salida").value = encrypt(text);
+  
