dd0bf7557e55f9ca4204838b3f6d7433cc7e39c6
JDanielFV <119628732+JDanielFV@users.noreply.github.com>
2022-12-28 00:02:21 -0600

    0.2

--- scrypt.js
+++ scrypt.js
@@ -1,28 +1,21 @@
-// Asignar controladores de eventos a los botones
 document.getElementById("botonEncriptar").onclick = function() {
-    // Obtener el texto de entrada y la salida
     let entrada = document.getElementById("textoEntrada").value;
     let salida = document.getElementById("textoSalida");
   
-    // Llamar a la función de encriptación con el texto de entrada como argumento y asignar el resultado a la propiedad value del elemento de salida
-    salida.value = encrypt(entrada);
+    salida.value = encrypt("entrada");
   }
   
   document.getElementById("botonDesencriptar").onclick = function() {
-    // Obtener el texto de entrada y la salida
     let entrada = document.getElementById("textoEntrada").value;
     let salida = document.getElementById("textoSalida");
   
-    // Llamar a la función de desencriptación con el texto de entrada como argumento y asignar el resultado a la propiedad value del elemento de salida
-    salida.value = decrypt(entrada);
+    salida.value = decrypt("entrada");
   }
   
-  // Función de encriptación
   function encrypt(text) {
     return btoa(text);
   }
   
-  // Función de desencriptación
   function decrypt(text) {
     return atob(text);
   }
