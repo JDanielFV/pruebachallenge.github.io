c7a9b2daa48fee9f59bb9eb2710e6f478621d135
JDanielFV <119628732+JDanielFV@users.noreply.github.com>
2022-12-28 11:45:11 -0600

    0.3

--- script.js
+++ script.js
@@ -7,16 +7,15 @@ function decrypt(text) {
 }
 
 document.getElementById("botonEncriptar").onclick = function() {
-    let entrada = document.getElementById("textoEntrada").value;
-    let salida = document.getElementById("textoSalida");
+    let entrada = document.getElementById("entrada").value;
+    let salida = document.getElementById("salida");
   
     salida.value = encrypt("entrada");
   }
   
   document.getElementById("botonDesencriptar").onclick = function() {
-    let entrada = document.getElementById("textoEntrada").value;
-    let salida = document.getElementById("textoSalida");
+    let entrada = document.getElementById("entrada").value;
+    let salida = document.getElementById("salida");
   
     salida.value = decrypt("entrada");
-  }
-  
+  } 
