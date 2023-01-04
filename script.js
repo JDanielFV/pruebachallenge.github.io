04b77746a07d48173eba405e66a41e9e9a78a4e4
JDanielFV <flores.vega.jesus@gmail.com>
2023-01-04 14:57:29 -0600

    0.7

--- script.js
+++ script.js
@@ -1,139 +1,96 @@
-var form = document.getElementById("forms");
-const vowels = ["e", "i", "a", "o", "u"];
-const vowelsKeys = ["enter", "imes", "ai", "ober", "ufat"]
+//agregando evento al boton de encriptar
+const botonEncriptar =document.querySelector("#botonEncriptar");
+botonEncriptar.addEventListener("submit",validando);
 
-//We reset the form when reload the page.
-document.body.onload = function () {
-    form.reset();
+//agregando evento al boton desencriptar
+const botonDesencriptar =document.querySelector("#botonDesencriptar");
+botonDesencriptar.addEventListener("click",validando2);
+
+//agregando evento al boton copiar
+const botonCopiar =document.querySelector("#botonCopiar");
+botonCopiar.addEventListener("click",copytext);
+
+
+let i ; //indice para recorrer vector reglas
+let y; //indice para recorrer vector desencriptador
+
+//funcion que copia el texto
+function copytext(t){
+t.preventDefault()
+//console.log("copiando");
+let content = document.getElementById("#textoSalida");
+    content.select();
+    let successful = document.execCommand('copy');
+    //console.log("esto fue lo que se copio:"+content)
 }
 
-//Valid that message has 140 number of characters
-function messageValidator(msg) {
-    if (msg.length <= 250) {
-        return true
-    } else {
-        return false
-    }
-}
-
-//replace letters in a word, the letter to be replaced, the value to be replaced and the word where it will be replaced are sent.
-function replaceLetters(bfLetter, atLetter, word) {
-    let re = new RegExp(bfLetter, 'g');
-    let newWord = word.replace(re, atLetter);
-    return newWord;
-}
-
-//Find the vowels in the word and apply the key corresponding tho the vowel
-function encryptWord(word) {
-    let newWord = "";
-    let letterMatch = "";
-
-    for (letter of word) {
-
-        for (let i = 0; i < vowels.length; i++) {
-
-            const element = vowels[i];
-            const elementKey = vowelsKeys[i];
-
-            if (letter == element) {
-                letterMatch = replaceLetters(element, elementKey, letter);
-                break;
-            } else {
-                letterMatch = letter;
-            }
-
-        }
-
-        newWord += letterMatch;
-    }
-    return newWord;
-
-}
-
-//Search for matches between keys and a word, then returns the decrypted word.
-function decryptWord(word) {
-    let newWord = word;
-
-    for (let i = 0; i < vowelsKeys.length; i++) {
-
-        const vKey = vowelsKeys[i];
-        const vowel = vowels[i];
-        const re = new RegExp(vKey, 'g');
-        let letterMatch = newWord.match(re);
-
-        if (letterMatch != null) {
-            newWord = replaceLetters(vKey, vowel, newWord);
-        }
-
-    }
-
-    return newWord;
-}
-
-//Gets the reference of the buttons with class .btn
-const buttons = document.querySelectorAll(".btn");
-
-//With this variable e control the actions of the application
-var selectedOption;
-
-//We ho through the buttons
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        console.log("El texto que tiene es: ",);
-        selectedOption = button.innerText;
-    })
-});
-
-//Functionality to copy text and send it to the clipboard
-function copyText(element) {
-    navigator.clipboard.writeText(element)
-    .then(() => {
-        alert('Texto copiado!');
-    })
-    .catch(() => {
-        alert('No se puede copiar el texto!',err);
-    })
-}
-
-//Trigger for button copy
-function btnCopy() {
-    let element = document.getElementById('msgResult').value;
-    copyText(element);
-}
-
-//Sends the resulting message to the DOM
-function messageResult(message) {
-    let encryptMessage = "";
-    encryptMessage = message.toString().replace(/,/g, " ");
-    console.log(encryptMessage);
-    document.getElementById('message-result').innerHTML = `<div class="message-result">
-    <textarea id="msgResult" cols="1" rows="5" maxlength="140" readonly>${encryptMessage}</textarea>
-    <button type="submit" onclick="btnCopy()" class="btn btn-copy">Copiar</button>
-    </div>
-    `;
-}
-
-
-//Application trigger
-form.onsubmit = function (e) {
+//funcion que encripta usando array 
+function validando(e){
     e.preventDefault();
-    let message = document.getElementById("input-message").value;
-    const wordArray = message.toLowerCase().split(" ");
+    //console.log("funcionando")
+    let frase = document.querySelector("#textoEntrada").value;
+    //console.log(frase)
+    frase=frase.toLowerCase()
+    let arr= frase.split('');//convierto la cadena en array
+    let texto_nuevo="";
+    const Vector_encripta=["ai","enter","imes","ober","ufat"];//vector que tiene las encriptaciones
+    //console.log(arr);
 
-    if (messageValidator) {
-
-        let resultArray = [];
-        for (word of wordArray) {
-
-            if (selectedOption == "Encriptar") {
-                resultArray.push(encryptWord(word));
-            }
-
-            if (selectedOption == "Desencriptar") {
-                resultArray.push(decryptWord(word));
-            }
-            messageResult(resultArray);
+    
+    for(i=0;i<arr.length;i++){
+       
+       
+        if(arr[i].includes("a")===true){          
+            texto_nuevo=texto_nuevo+Vector_encripta[0];
+                  
+        }else if(arr[i].includes("e")===true){
+           
+            texto_nuevo=texto_nuevo+Vector_encripta[1];
         }
-
+        else if(arr[i].includes("i")===true){
+            
+            texto_nuevo=texto_nuevo+Vector_encripta[2];
+        }
+        else if(arr[i].includes("o")===true){
+            
+            texto_nuevo=texto_nuevo+Vector_encripta[3];
+        }
+        else if(arr[i].includes("u")===true){
+            
+            texto_nuevo=texto_nuevo+Vector_encripta[4];
+        }
+        else{
+            texto_nuevo=texto_nuevo+arr[i];
+            
+        }
+             
+      
+        
     }
+
+    texto_nuevo=texto_nuevo.toLowerCase();
+    //para mostrar en el mismo formulario
+    const respuesta =document.getElementById("#textoSalida")
+    respuesta.textContent=`${texto_nuevo}`;
+}
+
+// funcion que desencripta la frase encriptada aqui se usa el metodo replace en cadenas
+function validando2(x){
+    x.preventDefault();
+    let ax = document.querySelector("#textoEntrada").value;
+let ix;
+let aux1=ax.toLowerCase();
+for(ix=0; ix<ax.length; ix++){
+       aux1= aux1.replace("ai","a");
+    aux1=aux1.replace("enter","e");
+    aux1=aux1.replace("imes","i");
+    aux1=aux1.replace("ober","o");
+    aux1=aux1.replace("ufat","u");
+}
+
+
+//para mostrar en el mismo formulario la cadena desencriptada
+const textoSalida =document.getElementById("#textoSalida")
+textoSalida.textContent=`${aux1}`;
+    
 }
