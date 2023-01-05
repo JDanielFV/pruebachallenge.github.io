911f7c9dce1cf9643cec32f10858aa5d19826145
JDanielFV <flores.vega.jesus@gmail.com>
2023-01-05 10:52:28 -0600

    0.9

--- script.js
+++ script.js
@@ -1,93 +1,52 @@
-//agregando evento al boton de encriptar
-const botonEncriptar =document.querySelector("#botonEncriptar");
-botonEncriptar.addEventListener("submit",validando);
+document.querySelector("#textoEntrada");
+const inputMensaje = document.querySelector("#textoEntrada")
+const inputResultado = document.querySelector("#textoSalida");
 
-//agregando evento al boton desencriptar
-const botonDesencriptar =document.querySelector("#botonDesencriptar");
-botonDesencriptar.addEventListener("submit",validando2);
-
-//agregando evento al boton copiar
-const botonCopiar =document.querySelector("#botonCopiar");
-botonCopiar.addEventListener("submit",copytext);
+const btnEncriptar = document.querySelector("#botonEncriptar");
+const btnDesencriptar = document.querySelector("#botonDesencriptar");
+const btnCopiar = document.querySelector("#botonCopiar");
 
 
-let i ; //indice para recorrer vector reglas
-let y; //indice para recorrer vector desencriptador
+function Encriptar(){
+    var mensaje = inputMensaje.value;
+    var mensajeEncriptado = mensaje
 
-//funcion que copia el texto
-function copytext(t){
-    navigator.clipboard.writeText("#textoSalida")
-    alert("copiado")
+    .replaceAll("e","enter")
+    .replaceAll("i","imes")
+    .replaceAll("o","ober")
+    .replaceAll("a","ai")
+    .replaceAll("u","ufat");
+    inputResultado.value = mensajeEncriptado;
 }
 
-//funcion que encripta usando array 
-function validando(e){
-    e.preventDefault();
-    //console.log("funcionando")
-    let frase = document.querySelector("#textoEntrada").value;
-    //console.log(frase)
-    frase=frase.toLowerCase()
-    let arr= frase.split('');//convierto la cadena en array
-    let texto_nuevo="";
-    const Vector_encripta=["ai","enter","imes","ober","ufat"];//vector que tiene las encriptaciones
-    //console.log(arr);
+function Desencriptar(){
+    var mensajeEncriptado = inputMensaje.value;
+    var mensaje = mensajeEncriptado
 
-    
-    for(i=0;i<arr.length;i++){
-       
-       
-        if(arr[i].includes("a")===true){          
-            texto_nuevo=texto_nuevo+Vector_encripta[0];
-                  
-        }else if(arr[i].includes("e")===true){
-           
-            texto_nuevo=texto_nuevo+Vector_encripta[1];
-        }
-        else if(arr[i].includes("i")===true){
-            
-            texto_nuevo=texto_nuevo+Vector_encripta[2];
-        }
-        else if(arr[i].includes("o")===true){
-            
-            texto_nuevo=texto_nuevo+Vector_encripta[3];
-        }
-        else if(arr[i].includes("u")===true){
-            
-            texto_nuevo=texto_nuevo+Vector_encripta[4];
-        }
-        else{
-            texto_nuevo=texto_nuevo+arr[i];
-            
-        }
-             
-      
-        
+    .replaceAll("enter","e")
+    .replaceAll("imes","i")
+    .replaceAll("ober","o")
+    .replaceAll("ai","a")
+    .replaceAll("ufat","u");
+    inputResultado.value = mensajeEncriptado;
+}
+
+/*function copiar(){
+    await navigator.clipboard.writeText("textoSalida");
+    console.log('contenido copiado')
+}*/
+
+function copiar(){
+    let text = document.querySelector("#textoSalida").innerHTML;
+    const copyContent = async () => {
+    try {
+        await navigator.clipboard.writeText(text);
+        console.log('Content copied to clipboard');
+    } catch (err) {
+        console.error('Failed to copy: ', err);
     }
-
-    texto_nuevo=texto_nuevo.toLowerCase();
-    //para mostrar en el mismo formulario
-    const respuesta =document.getElementById("#textoSalida")
-    respuesta.textContent=`${texto_nuevo}`;
+    }
 }
-
-
-// funcion que desencripta la frase encriptada aqui se usa el metodo replace en cadenas
-function validando2(x){
-    x.preventDefault();
-    let ax = document.querySelector("#textoEntrada").value;
-let ix;
-let aux1=ax.toLowerCase();
-for(ix=0; ix<ax.length; ix++){
-       aux1= aux1.replace("ai","a");
-    aux1=aux1.replace("enter","e");
-    aux1=aux1.replace("imes","i");
-    aux1=aux1.replace("ober","o");
-    aux1=aux1.replace("ufat","u");
-}
-
-
-//para mostrar en el mismo formulario la cadena desencriptada
-const textoSalida =document.getElementById("#textoSalida")
-textoSalida.textContent=`${aux1}`;
-    
-}
+btnEncriptar.onclick = Encriptar;
+btnDesencriptar.onclick = Desencriptar;
+btnCopiar.onclick = copiar;
