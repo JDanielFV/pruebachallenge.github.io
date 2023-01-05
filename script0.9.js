112d8320af1cac77bc2628d1bac04d9ebb13595c
JDanielFV <flores.vega.jesus@gmail.com>
2023-01-05 01:48:10 -0600

    0.9

---
+++ script0.9.js
@@ -0,0 +1,39 @@
+document.querySelector("#textoEntrada");
+const inputResultado = document.querySelector("#textoSalida");
+
+const btnEncriptar = document.querySelector("#botonEncriptar");
+const btnDesencriptar = document.querySelector("#botonDesencriptar");
+const btnCopiar = document.querySelector("#botonCopiar");
+
+
+function Encriptar(){
+var mensaje = inputMensaje.value;
+var mensajeEncriptado = mensaje
+
+.replaceAll("e","enter")
+.replaceAll("i","imes")
+.replaceAll("o","ober")
+.replaceAll("a","ai")
+.replaceAll("u","ufat");
+inputResultado.value = mensajeEncriptado;
+}
+
+function Desencriptar(){
+    var mensajeEncriptado = inputMensaje.value;
+    var mensaje = mensajeEncriptado
+
+    .replaceAll("enter","e")
+    .replaceAll("imes","i")
+    .replaceAll("ober","o")
+    .replaceAll("ai","a")
+    .replaceAll("ufat","u");
+    inputResultado.value = mensajeEncriptado;
+    }
+function copiar(){
+    var mensajeEncriptado = inputResultado.value;
+    navigator.clipboard.writeText();
+}
+
+btnEncriptar.onclick = Encriptar;
+btnDesencriptar.onclick = Desencriptar;
+btnCopiar.onclick = copiar;
